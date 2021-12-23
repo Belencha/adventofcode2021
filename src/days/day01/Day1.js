82556d1f5fccd56b6f02d243e9e6bc0e484d0f01
Belén Moreno Hurtado <bbmh@gmv.com>
2021-12-23 15:04:12 +0100

    Day 1 Second half done with nonsense logs that need fixing

--- src/days/day01/Day1.js
+++ src/days/day01/Day1.js
@@ -3,6 +3,7 @@ import raw from "./day1Input.txt";
 const Day1 = () => {
     let inputData = "";
     let numberOfIncreases = 0;
+    let numberOfWindowsIncreases = 0;
 
     const countIncreasedMeasures = (currentMeasure, measures, total) => {
         if (measures.length === 0) {
@@ -11,8 +12,25 @@ const Day1 = () => {
         if (measures[0] > currentMeasure) {
             total++;
         }
-        console.log(total);
+        /*console.log(total);*/
         countIncreasedMeasures(measures.shift(), measures, total);
+    }
+
+    function countThreeWindowIncreasedMeasures(previousMeasure, measures, total) {
+        let partialSum = 0;
+        let currentNode = 0;
+
+        if (measures.length < 3) {
+            return total;
+        } else {
+            currentNode = measures.shift();
+            partialSum = currentNode + measures[0] + measures[1];
+            if (partialSum > previousMeasure && previousMeasure !== 0) {
+                total++;
+            }
+            console.log("Total: " + total);
+            return countThreeWindowIncreasedMeasures(partialSum, measures, total);
+        }
     }
 
     fetch(raw)
@@ -23,9 +41,15 @@ const Day1 = () => {
                 return parseInt(each.match(regex)[0]);
             });
             numberOfIncreases = countIncreasedMeasures(inputData[0], inputData, 0);
-            console.log(`${ numberOfIncreases } are larger than the previous measurement`); /* UNDEFINEEEEEED, oh shit, tengo que aprender esto */
+            numberOfWindowsIncreases = countThreeWindowIncreasedMeasures(0, inputData, 0);
+
+            /* UNDEFINEEEEEED, oh shit, tengo que aprender esto */
+            console.log(`${ numberOfIncreases } are larger than the previous measurement`); 
+
+            console.log(`${ numberOfWindowsIncreases } are larger than the previous windows`);
+
         });
-        const message = `Day 1 done`;
+        const message = `Day 1 half done`;
         return message;
     
 }
